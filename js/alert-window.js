854a654066acec235f009fe0a825e7a20774a2e4
koldovsky <koldovsky@yahoo.com>
2018-03-09 17:31:17 +0000

    Refactored

---
+++ js/alert-window.js
@@ -0,0 +1,14 @@
+function showAlert(message, success = true) {
+    const alertTemplate = 
+        `<div class="alert alert-dismissible ${success ? 'alert-success' : 'alert-danger'}" role="alert">
+          <strong>${message}</strong> 
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+          </button>
+        </div>`;
+    const alertElement = $('body').append(alertTemplate);
+    alertElement.alert();
+    setTimeout(() => {
+        $('.alert').alert('close');
+    }, 1000);
+}
